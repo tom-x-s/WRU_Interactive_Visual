6b1d3251ba79d656d0182f40dd3bacedffa8d08b
Manon <NonPon97@gmail.com>
2020-06-04 12:43:53 +0200

    Wagentje rijd met kleur collision

---
+++ Assets/Scenes/kleur.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kleur : MonoBehaviour
+{
+    int colorPicker = 0;
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        colorPicker = Random.Range(0, 10);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        switch (colorPicker)
+        {
+            case 0: rend.material.color = Color.white; break;
+            case 1: rend.material.color = Color.cyan; break;
+            case 2: rend.material.color = Color.blue; break;
+            case 3: rend.material.color = Color.black; break;
+            case 4: rend.material.color = Color.red; break;
+            case 5: rend.material.color = Color.green; break;
+            case 6: rend.material.color = Color.grey; break;
+            case 7: rend.material.color = Color.magenta; break;
+            case 8: rend.material.color = Color.yellow; break;
+            case 9: rend.material.color = Color.gray; break;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        colorPicker = Random.Range(0, 10);
+        switch (colorPicker)
+        {
+            case 0: rend.material.color = Color.white; break;
+            case 1: rend.material.color = Color.cyan; break;
+            case 2: rend.material.color = Color.blue; break;
+            case 3: rend.material.color = Color.black; break;
+            case 4: rend.material.color = Color.red; break;
+            case 5: rend.material.color = Color.green; break;
+            case 6: rend.material.color = Color.grey; break;
+            case 7: rend.material.color = Color.magenta; break;
+            case 8: rend.material.color = Color.yellow; break;
+            case 9: rend.material.color = Color.gray; break;
+        }
+    }
+}
