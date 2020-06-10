e6a9a1df9ef0a824b8c8a0e8792d7fb64fb9babf
Manon <NonPon97@gmail.com>
2020-06-10 11:15:17 +0200

    Boing wanneer je andere cirkel raakt

---
+++ Assets/Scenes/Scripts/boing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Add this Script Directly to The Death Zone
+public class boing : MonoBehaviour
+{
+    public AudioClip saw;    // Add your Audi Clip Here;
+                             // This Will Configure the  AudioSource Component; 
+                             // MAke Sure You added AudioSouce to death Zone;
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = saw;
+    }
+
+    void OnTriggerEnter(Collider other)  //Plays Sound Whenever collision detected
+    {
+        if (other.gameObject.name == "Sphere")
+        {
+
+            GetComponent<AudioSource>().Play();
+        }
+    }
+    // Make sure that deathzone has a collider, box, or mesh.. ect..,
+    // Make sure to turn "off" collider trigger for your deathzone Area;
+    // Make sure That anything that collides into deathzone, is rigidbody;
+}
