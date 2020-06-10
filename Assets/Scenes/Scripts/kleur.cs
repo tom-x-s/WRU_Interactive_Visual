2eb0691145cf76e19ccd3580680587e2617f48a2
Tom-X-S <txsomhorst@gmail.com>
2020-06-10 10:22:53 +0200

    File cleanup
    Bestanden in het project ingedeeld in mappen voor beter overzicht

---
+++ Assets/Scenes/Scripts/kleur.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kleur : MonoBehaviour
+{
+    int colorPicker = 0;
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Sphere")
+        {
+            colorPicker = Random.Range(0, 0);
+            switch (colorPicker)
+            {
+                case 0: rend.material.color = Color.green; break;
+
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Sphere")
+        {
+            colorPicker = Random.Range(0, 0);
+            switch (colorPicker)
+            {
+                case 0: rend.material.color = Color.red; break;
+
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+       
+            colorPicker = Random.Range(0, 0);
+            switch (colorPicker)
+            {
+                case 0: rend.material.color = Color.green; break;
+
+            }
+        
+    }
+}
