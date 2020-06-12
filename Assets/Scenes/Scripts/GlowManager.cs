f51c02c5a5097cd785a77e522fd852a9fe18f1e1
Manon <NonPon97@gmail.com>
2020-06-12 12:40:10 +0200

    de gangpad-scene is nu strepen ipv hele vloer kleur veranderend
    Kan nu ook veranderen op material color ipv gewoon color

---
+++ Assets/Scenes/Scripts/GlowManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowManager : MonoBehaviour
+{
+    public Material glow, nonglow;
+    public bool isGlowing = true;
+
+    public void ToggleGlow()
+    {
+        if (isGlowing)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = nonglow;
+            isGlowing = false;
+        }
+        else
+        {
+            gameObject.GetComponent<MeshRenderer>().material = glow;
+            isGlowing = false;
+        }
+    }
+}
--- Assets/Scenes/Scripts/gangpadlicht.cs
+++ Assets/Scenes/Scripts/gangpadlicht.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class gangpadlicht : MonoBehaviour
 {
     public int countCollisions = 0;
-    int colorPicker = 0;
-    Renderer rend;
+    //int colorPicker = 0;
+    //Renderer rend;
+    public Material ColourGreen;
+    public Material ColourRed;
 
     void Start()
     {
-        rend = GetComponent<Renderer>();
-        rend.material.color = Color.green;
+        //rend = GetComponent<Renderer>();
+       // rend.material.color = Color.green;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -34,22 +36,24 @@ public class gangpadlicht : MonoBehaviour
     {
         if (countCollisions < 2)
         {
-            colorPicker = Random.Range(0, 0);
-            switch (colorPicker)
-            {
-                case 0: rend.material.color = Color.green; break;
+            //colorPicker = Random.Range(0, 0);
+            //switch (colorPicker)
+            //{
+            //     case 0: rend.material.color = Color.green; break;
 
-            }
+            // }
+            GetComponent<MeshRenderer>().material = ColourGreen;
         }
         if (countCollisions == 2)
         {
-            colorPicker = Random.Range(0, 0);
-            switch (colorPicker)
-            {
-                case 0: rend.material.color = Color.red; break;
+            //colorPicker = Random.Range(0, 0);
+            // switch (colorPicker)
+            // {
+            //     case 0: rend.material.color = Color.red; break;
 
-            }
-           
+            // }
+            GetComponent<MeshRenderer>().material = ColourRed;
+
         }
         if (countCollisions > 2)
         {
