736c3f22c069b5aed7d0853b5848aeb1f4b6e402
WinnieNH <winniehoogakker@gmail.com>
2020-06-15 14:22:32 +0200

    Boodschappen verzamelen 90% af

--- Assets/Scenes/Scripts/Oppakken.cs
+++ Assets/Scenes/Scripts/Oppakken.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Oppakken : MonoBehaviour
 {
+    public GameObject PObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("hallo");
+        if (Input.GetKey(KeyCode.F))
+        {
+            this.transform.position = new Vector3(PObject.transform.position.x, PObject.transform.position.y + 0.6f, PObject.transform.position.z + 0.5f);
+            this.transform.SetParent(PObject.transform);
+            GetComponent<Collider>().enabled = false; 
+        }
     }
 }
