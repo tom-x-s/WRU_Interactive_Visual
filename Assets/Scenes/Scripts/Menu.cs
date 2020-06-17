768c86e23a67e97351c85ecfe58149ab9f94633d
Tom-X-S <txsomhorst@gmail.com>
2020-06-17 10:56:51 +0200

    Main Menu
    Menu gemaakt om tussen verschillende test scenes te switchen.

---
+++ Assets/Scenes/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void NextScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
